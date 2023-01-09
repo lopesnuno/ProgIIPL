04fd73b76145ad3dc13975eb0b579029bcce0f11
lopesnuno <lopesnuno@ipvc.pt>
2023-01-09 15:46:00 +0000

    Change info method

--- src/GUI/ListarDadosCliente.java
+++ src/GUI/ListarDadosCliente.java
@@ -1,7 +1,10 @@
 package GUI;
 
 import Entidades.Cliente;
+import Exceptions.AlterarDadosException;
 import Metodos.MetodosAnon;
+import Metodos.MetodosCliente;
+import Repositorio.RepositorioSerializable;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,19 +22,17 @@ public class ListarDadosCliente extends JFrame {
     private JButton BotaoAlterar;
     private JButton BotaoLimpar;
     private JButton BotaoVoltar;
-    private final MetodosAnon metodos;
 
     public ListarDadosCliente(JFrame frame, Cliente cliente) {
         frame.setTitle("Alterar Dados");
         frame.setPreferredSize(new Dimension(500, 500));
-
-        metodos = new MetodosAnon();
 
         frame.add(DadosCliente);
         frame.pack();
         frame.setVisible(true);
         limparDados();
         voltar(frame);
+        alterarDados(frame, cliente);
 
         username.setText(cliente.getUsername());
         password.setText(cliente.getPassword());
@@ -41,6 +42,46 @@ public class ListarDadosCliente extends JFrame {
         telefone.setText(String.valueOf(cliente.getTelefone()));
         morada.setText(String.valueOf(cliente.getMorada()));
         localidade.setText(String.valueOf(cliente.getLocalidade()));
+    }
+
+    public void alterarDados(JFrame frame, Cliente cliente) {
+        BotaoAlterar.addActionListener(e -> {
+            int telefone = 0, nif = 0, numCC = 0;
+            String username = this.username.getText();
+            String password = String.valueOf(this.password.getPassword());
+            String nome = this.nome.getText();
+            String morada = this.morada.getText();
+            String localidade = this.localidade.getText();
+
+            try {
+                telefone = Integer.parseInt(this.telefone.getText());
+                nif = Integer.parseInt(this.nif.getText());
+                numCC = Integer.parseInt(this.numCC.getText());
+            } catch (NumberFormatException err) {
+                JOptionPane.showMessageDialog(null, "Não pode introduzir letras num campo de números !");
+                return;
+            }
+
+            try {
+                cliente.setUsername(username);
+                cliente.setPassword(password);
+                cliente.setNome(nome);
+                cliente.setNumCC(numCC);
+                cliente.setNIF(nif);
+                cliente.setTelefone(telefone);
+                cliente.setMorada(morada);
+                cliente.setLocalidade(localidade);
+
+                MetodosCliente.alterarDadosCliente(cliente);
+                RepositorioSerializable.writeUsers();
+
+                JOptionPane.showMessageDialog(null, "Dados alterados com sucesso.");
+                DadosCliente.setVisible(false);
+                new ClienteRegistado(frame);
+            } catch (AlterarDadosException err) {
+                JOptionPane.showMessageDialog(null, err.getMessage());
+            }
+        });
     }
 
     public void limparDados() {
