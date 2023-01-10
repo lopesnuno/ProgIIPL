3960757c6d26cd4014dfdc055b93754dc6fa65b0
MiguelOliveira63 <117329614+MiguelOliveira63@users.noreply.github.com>
2023-01-10 12:20:22 +0000

    ta top

--- src/Metodos/MetodosAdmin.java
+++ src/Metodos/MetodosAdmin.java
@@ -1,7 +1,9 @@
 package Metodos;
 
+import Entidades.Carro;
 import Entidades.User;
 import Exceptions.AlterarDadosException;
+import Exceptions.JaExisteCarroException;
 import Exceptions.JaExisteUserException;
 import Repositorio.Repositorio;
 import Repositorio.RepositorioSerializable;
@@ -52,8 +54,56 @@ public class MetodosAdmin {
         }
     }
 
+    public static void addCarro(Carro carro) throws JaExisteCarroException {
+        boolean existe = false;
+        for (Carro u : Repositorio.getInstance().getUsers()) {
+            if (u.getMatricula().equals(carro.getMatricula()) || u.getMarca() == carro.getMarca()) {
+                existe = true;
+                break;
+            }
+        }
+
+        if (existe) {
+            throw new JaExisteCarroException("Este Carro já existe !");
+        } else {
+            Repositorio.getInstance().getCarros().add(carro);
+            RepositorioSerializable.writeCarros();
+        }
+    }
+
     public static void removerUser(User user) {
         Repositorio.getInstance().getUsers().remove(user);
         RepositorioSerializable.writeUsers();
+    }
+
+    public static Carro selectCarroPorMatricula(String matricula) {
+        for (Carro u : Repositorio.getInstance().getCarros()) {
+            if (u.getMatricula().equals(matricula)) {
+                return u;
+            }
+        }
+
+        return null;
+    }
+
+    public static void alterarDadosCarro(Carro carro) throws AlterarDadosException {
+        for (Carro u : Repositorio.getInstance().getCarros()) {
+            if (u.getMatricula().equals(carro.getMatricula()) && u.getMatricula() == carro.getMatricula()) {
+                u.setMatricula(carro.getMatricula());
+                u.setMarca(carro.getMarca());
+                u.setModelo(carro.getModelo());
+                u.setAno(carro.getAno());
+                u.setPreco(carro.getPreco());
+                return;
+            }
+        }
+
+        throw new AlterarDadosException("Não foi possível encontrar o carro que queria alterar.");
+    }
+
+    public static void removerCarro(Carro carro) {
+        Repositorio.getInstance().getCarros().remove(carro);
+        RepositorioSerializable.writeCarros();
+
     }
 }
